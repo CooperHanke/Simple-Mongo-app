466c765d69e67323469349064a064d8dba5b57ea
Cooper Hanke <cooper.hanke@gmail.com>
2018-06-06 11:39:24 -0500

    init commit after creating Journal class

---
+++ src/main/java/guru/cooperhanke/mongodb/SimpleMongoAppApplication.java
@@ -0,0 +1,12 @@
+package guru.cooperhanke.mongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleMongoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleMongoAppApplication.class, args);
+	}
+}
